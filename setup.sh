7553e09e9b02ef20c67802caf330d492df28ce4d
Ryan Huang <ryanhuang@cs.ucsd.edu>
2016-11-21 17:16:41 -0800

    update setup

--- setup.sh
+++ setup.sh
@@ -23,24 +23,40 @@ backup()
   fi
 }
 
-setup()
+autocomplete()
 {
-  parent="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-  if [ ! -f $parent/setup.sh ]; then
-    echo "I exist in an unknown place"
-    exit 1
-  fi
-  echo "Backuping vimrc and vim"
-  backup ~/.vimrc
-  backup ~/.vim
-  echo "Done"
-  $maybe mv $parent ~/.vim
-  $maybe cp ~/.vim/vimrc ~/.vimrc
-  $maybe cd ~/.vim
-  $maybe git submodule update --init --recursive
   $maybe sudo apt-get install cmake python-dev
   $maybe cd ~/.vim/bundle/YouCompleteMe
   $maybe ./install.py --clang-completer
+}
+
+setup()
+{
+  curr="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+  parent="$(dirname "$curr")"
+  if [ ! -f $curr/setup.sh -o ! -d $parent/vimrc ]; then
+    echo "I exist in an unknown place"
+    exit 1
+  fi
+  echo "$parent"
+  echo "Backuping vimrc and vim"
+  backup ~/.vimrc
+  backup ~/.vim
+  echo "Updating vimrc and vim"
+  $maybe cp -r $parent/vimrc ~/.vim
+  $maybe cp ~/.vim/vimrc ~/.vimrc
+  $maybe cd ~/.vim
+  echo "Updating submodules"
+  $maybe git submodule update --init --recursive
+  read -r -p "Setup YouCompleteMe (code completion)? [y/N] " response
+  case $response in
+    [yY]|yes|YES|Yes)
+      autocomplete
+      ;;
+    *)
+      ;;
+  esac
+  echo "Done. You can delete ~/vimrc now"
 }
 
 case $1 in
